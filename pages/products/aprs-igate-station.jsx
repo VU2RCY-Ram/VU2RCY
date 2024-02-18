9dea31f2f94a0e3792b55617a329adad51a57412
vu2rcy <vu2rcy@gmail.com>
2024-02-18 14:12:56 +0530

    wip /products/pages....

---
+++ pages/products/aprs-igate-station.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Head from "next/head";
+import Breadcrumb from "@/components/Breadcrumb";
+
+function AprsIgateStation() {
+  let UrlData = ["home", "products", "aprs-igate-station"];
+
+  return (
+    <div className="px-8 sm:px-14 md:px-24 lg:px-30 lg:mb-12">
+      <Head>
+        <title>HF ✦ VU2RCY </title>
+        <link rel="icon" href="./logo.png" />
+      </Head>
+      <main className="  max-w-screen ">
+        <div className=" pt-24 container mx-auto p-4">
+          <h1
+            className={
+              "selection:text-black/40 capitalize dark:selection:text-white/40 font-deca bg-gradient-to-tr from-teal-300 to-blue-500 bg-clip-text text-transparent items-center mx-auto text-lg md:text-2xl lg:text-3xl xl:text-4xl font-bold"
+            }
+          >
+            aprs igate station
+          </h1>
+          <Breadcrumb data={UrlData} />
+          <br />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default AprsIgateStation;
--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -24,6 +24,79 @@ export default function Work() {
     };
   }, []);
 
+  let data = [
+    {
+      title: "APRS IGATE Station",
+      description:
+        "An APRS IGATE Station serves as a gateway between the Automatic Packet Reporting System (APRS) and the internet, enabling the exchange of APRS data packets. It enhances APRS network coverage and facilitates data transmission to online APRS servers.",
+      image: "./homebrew/cw-icon.jpg",
+      toUrl: "/aprs-igate-station",
+    },
+    {
+      title: "APRS IGATE Weather Station",
+      description:
+        "An APRS IGATE Weather Station combines the functionality of an APRS IGATE Station with weather monitoring capabilities. It receives and forwards APRS packets while also collecting and transmitting weather data, such as temperature, humidity, and atmospheric pressure, to APRS networks.",
+      image: "./homebrew/cw-icon.jpg",
+      toUrl: "",
+    },
+    {
+      title: "APRS tracker",
+      description:
+        "An APRS tracker is a device designed to broadcast its GPS location via the Automatic Packet Reporting System (APRS). It's commonly used in amateur radio and outdoor activities to provide real-time location updates, allowing users to track movement or assets remotely.",
+      image: "./homebrew/cw-icon.jpg",
+      toUrl: "",
+    },
+    {
+      title: "lora satellite ground station",
+      description:
+        "A LoRa satellite ground station is a facility equipped to communicate with LoRa satellites. It facilitates two-way communication between ground-based LoRa devices and satellites in orbit, enabling long-range, low-power data transmission for applications such as IoT connectivity and remote sensing.",
+      image: "./homebrew/cw-icon.jpg",
+      toUrl: "",
+    },
+    {
+      title: "Digiface pro",
+      description:
+        "The Digiface Pro is a professional audio interface designed for high-performance recording and playback tasks. It offers a range of connectivity options, low-latency operation, and advanced features tailored to professional audio production environments, making it ideal for studio recording, live sound, and broadcast applications.",
+      image: "./homebrew/cw-icon.jpg",
+      toUrl: "",
+    },
+    {
+      title: "Network transceiver",
+      description:
+        "A network transceiver is a device that combines the functions of a transmitter and a receiver for data communication over a network. It enables bidirectional data transmission between connected devices, facilitating network communication protocols such as Ethernet, Wi-Fi, or cellular connectivity in various applications.",
+      image: "./homebrew/cw-icon.jpg",
+      toUrl: "",
+    },
+    {
+      title: "Lora APRS IGaTE Station",
+      description:
+        "A LoRa APRS IGATE Station integrates LoRa technology with APRS functionality, serving as a gateway between LoRa devices and APRS networks. It enables the transmission of LoRa data packets to APRS infrastructure, extending the reach of LoRa-based applications and enhancing network interoperability.",
+      image: "./homebrew/cw-icon.jpg",
+      toUrl: "",
+    },
+    {
+      title: "MMDVM",
+      description:
+        "The Multi-Mode Digital Voice Modem (MMDVM) is a hardware and software solution for connecting various digital voice modes, such as DMR, D-STAR, and C4FM, to analog radio systems. It facilitates cross-mode communication and interoperability between different digital voice protocols, enhancing flexibility in amateur radio operations.",
+      image: "./homebrew/cw-icon.jpg",
+      toUrl: "",
+    },
+    {
+      title: "Rig control cube [cat cable]",
+      description:
+        "A rig control cube, connected via a CAT (Computer-Aided Transceiver) cable, is a device used in amateur radio setups to interface with transceivers for remote control and monitoring. It allows users to adjust parameters such as frequency, mode, and power output from a computer or external controller.",
+      image: "./homebrew/cw-icon.jpg",
+      toUrl: "",
+    },
+    {
+      title: "Cw interface [serial/usb]",
+      description:
+        "A CW (Continuous Wave) interface, available in both serial and USB configurations, facilitates Morse code communication in amateur radio operations. It connects Morse code input devices, such as paddles or keys, to transceivers or computers, enabling the transmission and reception of Morse code signals.",
+      image: "./homebrew/cw-icon.jpg",
+      toUrl: "",
+    },
+  ];
+
   return (
     <div className="px-10 sm:px-20 md:px-32 md:-mb-52 lg:mb-0 lg:px-60 mx-auto max-w-[75rem]">
       <Head>
@@ -72,7 +145,7 @@ export default function Work() {
           </div>
         </div>
         <div className="mt-20 mb-20">
-          <Accordion />
+          <Accordion data={data} />
         </div>
       </main>
     </div>
